470e996892b165fe12cbfbaf44bf00c92625617f
Iain Buclaw <ibuclaw@gdcproject.org>
2019-01-10 19:29:20 +0100

    testsuite: Add TEST_OUTPUT to most fail compilation tests

--- test/fail_compilation/fail5634.d
+++ test/fail_compilation/fail5634.d
@@ -1,3 +1,9 @@
+/*
+TEST_OUTPUT:
+---
+fail_compilation/fail5634.d(8): Error: only one `main` allowed. Previously found `main` at fail_compilation/fail5634.d(7)
+---
+*/
 void main() { }
 void main() { }
 
